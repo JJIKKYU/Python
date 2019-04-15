c17526b8988a19d567267daa180e963793589304
JIKKYU <jjikkyu@naver.com>
2019-04-15 21:03:49 +0900

    ADD : jump to python 2-1 practice

---
+++ JumpToPython/02-1_문제1.py
@@ -0,0 +1,9 @@
+# [문제1] 점수의 평균
+
+jumsoo = [80, 75, 55]
+avg = 0
+
+for idx in jumsoo:
+    avg += idx
+avg /= len(jumsoo)
+    
---
+++ JumpToPython/02-1_문제2.py
@@ -0,0 +1,9 @@
+# [문제2] 자연수의 홀짝
+import random
+
+a = random.randint(0,50)
+
+if ( a%2 == 0):
+    print("짝수입니다.")
+if ( a%2 == 1):
+    print("홀수입니다.")
